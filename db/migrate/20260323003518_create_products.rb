188b92889f5c2a1b4f8882382137d521f04dd220
Yirui-Huang227 <huangyr227@gmail.com>
2026-03-22 19:41:34 -0500

    Generate Category model

--- db/migrate/20260323003518_create_products.rb
+++ db/migrate/20260323003518_create_products.rb
@@ -3,7 +3,7 @@ class CreateProducts < ActiveRecord::Migration[7.1]
     create_table :products do |t|
       t.string :name
       t.text :description
-      t.decimal :price
+      t.decimal :price, precision: 10, scale: 2
       t.boolean :on_sale
       t.integer :quantity
 
