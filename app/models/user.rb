c25825c75c543277805661e87abfb00862d32382
Yirui-Huang227 <huangyr227@gmail.com>
2026-03-22 19:55:49 -0500

    Add fields to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,30 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  belongs_to :province
+  has_many :orders
+
+  # first_name / last_name (string)
+  validates :first_name, presence: true,
+                         length: { minimum: 2, maximum: 50 }
+
+  validates :last_name, presence: true,
+                        length: { minimum: 2, maximum: 50 }
+
+  # address (string)
+  validates :address, presence: true,
+                      length: { minimum: 5 }
+
+  # city (string)
+  validates :city, presence: true
+
+  # postal_code (string)
+  validates :postal_code, presence: true,
+                          format: { with: /\A[A-Za-z]\d[A-Za-z] ?\d[A-Za-z]\d\z/,
+                                    message: "invalid Canadian postal code" }
+
+  # province_id (integer FK)
+  validates :province_id, presence: true,
+                          numericality: { only_integer: true }
 end
