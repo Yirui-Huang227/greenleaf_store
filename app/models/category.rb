ec0722c22d1fac236dba14e1b18ef43e3386aaba
Yirui-Huang227 <huangyr227@gmail.com>
2026-03-22 19:44:35 -0500

    Generate ProductCategory model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,12 @@
 class Category < ApplicationRecord
+  has_many :product_categories
+  has_many :products, through: :product_categories
+
+  # name (string)
+  validates :name, presence: true,
+                   length: { minimum: 2, maximum: 100 }
+
+  # description (text)
+  validates :description, presence: true,
+                          length: { minimum: 5 }
 end
