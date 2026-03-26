fc7c2dfb925bca048ba6cbb811039371fa29748a
Yirui-Huang227 <huangyr227@gmail.com>
2026-03-26 17:39:15 -0500

    Add search form partial with keyword search and category dropdown

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,14 @@
 class ProductsController < ApplicationController
   def index
     @products = Product.includes(:categories).order(created_at: :desc)
+
+    if params[:keyword].present?
+      @products = @products.where("name LIKE ? OR description LIKE ?", "%#{params[:keyword]}%", "%#{params[:keyword]}%")
+    end
+
+    if params[:category_id].present?
+      @products = @products.joins(:categories).where(categories: { id: params[:category_id] })
+    end
   end
 
   def show
