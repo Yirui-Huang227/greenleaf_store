4bb27d115c954fcf3bb1e2f5d7efe0326fe58864
Yirui-Huang227 <huangyr227@gmail.com>
2026-03-26 17:03:27 -0500

    Implement homepage displaying featured products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,9 @@
 class ProductsController < ApplicationController
   def index
+    @products = Product.includes(:categories).order(created_at: :desc)
   end
 
   def show
+    @product = Product.find(params[:id])
   end
 end
