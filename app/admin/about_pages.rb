14412e9c624c6ee8460e2e6dddf77a8b5112b7a7
Yirui-Huang227 <huangyr227@gmail.com>
2026-03-26 16:01:26 -0500

    Add ActiveAdmin support for AboutPage

--- app/admin/about_pages.rb
+++ app/admin/about_pages.rb
@@ -14,5 +14,53 @@ ActiveAdmin.register AboutPage do
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
-  
+
+  permit_params :title, :about, :contact
+
+  index do
+    selectable_column
+    id_column
+    column :title
+    column :admin_user
+    column :created_at
+    actions
+  end
+
+  filter :title
+  filter :admin_user
+  filter :created_at
+
+  form do |f|
+    f.inputs do
+      f.input :title
+      f.input :about
+      f.input :contact
+    end
+    f.actions
+  end
+
+  show do
+    attributes_table do
+      row :id
+      row :title
+      row :about
+      row :contact
+      row :admin_user
+      row :created_at
+      row :updated_at
+    end
+  end
+
+  controller do
+    def create
+      @about_page = AboutPage.new(permitted_params[:about_page])
+      @about_page.admin_user = current_admin_user
+      create!
+    end
+
+    def update
+      resource.admin_user = current_admin_user
+      update!
+    end
+  end
 end
--- app/models/about_page.rb
+++ app/models/about_page.rb
@@ -15,4 +15,12 @@ class AboutPage < ApplicationRecord
   # admin_user_id (integer FK)
   validates :admin_user_id, presence: true,
                             numericality: { only_integer: true }
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["id", "title", "about", "contact", "admin_user_id", "created_at", "updated_at"]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    ["admin_user"]
+  end
 end
